cde4c148f9c2027f243f51f85813d617f1de5969
Bret Baptist <bbaptist@iexposure.com>
2019-09-18 18:35:52 -0500

    Fix paths and add envvar support

--- bin/common.sh
+++ bin/common.sh
@@ -1,9 +1,24 @@
+#!/usr/bin/env bash
+
+export_env_dir() {
+  env_dir=$1
+  whitelist_regex=${2:-''}
+  blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH|JAVA_OPTS)$'}
+  if [ -d "$env_dir" ]; then
+    for e in $(ls $env_dir); do
+      echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+      export "$e=$(cat $env_dir/$e)"
+      :
+    done
+  fi
+}
+
 error() {
   echo " !     $*" >&2
   exit 1
 }
 
-status() {
+topic() {
   echo "-----> $*"
 }
 
